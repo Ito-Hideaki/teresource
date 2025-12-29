4f2341c22ac6a9ad3a27e4925e9bf341e553db95
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2025-12-29 23:27:39 +0900

    use MinoQueueView in ViewController

--- src/teresource/scenes/play/view/viewcontroller.js
+++ src/teresource/scenes/play/view/viewcontroller.js
@@ -3,12 +3,14 @@ import { BoardView } from "./boardview";
 import { BoardDeco } from "./boarddeco";
 import { GameViewContext } from "../infra/context";
 import { PlayScene } from "../../play";
+import { MinoQueueView } from "./subminoview";
 
 /** Represents the game view for each player */
 export class ViewController {
     #boardView;
     #boardDeco;
     #boardContainer;
+    #minoQueueView;
     set x(x) {
         this.#boardContainer.x = x;
     }
@@ -24,12 +26,14 @@ export class ViewController {
         this.#boardContainer = gvContext.boardContainer;
         this.#boardDeco = new BoardDeco(scene, 30, gvContext);
         this.#boardView = new BoardView(scene, 30, gvContext);
+        this.#minoQueueView = new MinoQueueView(scene, gvContext);
     }
 
     /** @param {number} deltaTime */
     update(deltaTime) {
         this.#boardDeco.update();
         this.#boardView.update();
+        this.#minoQueueView.update();
         // let rad = Math.atan2((this.#boardContainer.y - 360) / 15, (this.#boardContainer.x - 540) / 50);
         // rad += 0.1;
         // this.#boardContainer.x = 540 + Math.cos(rad) * 50;
