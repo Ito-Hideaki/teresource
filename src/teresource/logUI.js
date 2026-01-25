b3be4767a9ee991df9e4b8c7537e54c460782cd4
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2026-01-25 23:25:17 +0900

    fix logUI bug 2

--- src/teresource/logUI.js
+++ src/teresource/logUI.js
@@ -4,13 +4,26 @@ export function createLogBox() {
     const box = document.createElement("div");
     box.classList.add("loguibox");
 
+    /** @type {HTMLElement[]} */ const labels = [];
+    for(let i = 0; i < 10; i++) {
+        const label = document.createElement("div");
+        labels.push(label);
+        label.textContent = "_";
+        label.classList.add("loguilabel");
+        box.appendChild(label);
+    }
+
+    function updateForNewSentence(sentence) {
+        labels.forEach((label, i) => {
+            if(i+1 == labels.length) label.innerHTML = sentence;
+            else label.innerHTML = labels[i+1].innerHTML;
+        });
+    }
+
     /** @param { string } sentence */
     function log(sentence) {
-        const label = document.createElement("div");
-        label.classList.add("loguilabel");
         const timeString = dayjs().format("hh:mm:ss.SSS");
-        label.innerHTML = `[${timeString}] ${sentence}`;
-        box.appendChild(label);
+        updateForNewSentence(`[${timeString}] ${sentence}`);
     }
 
     return { box, log }
