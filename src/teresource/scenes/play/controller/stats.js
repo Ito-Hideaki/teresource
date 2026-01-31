036cb91a16ae3794d7ae49d54067258c22301c0c
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2026-01-31 22:46:21 +0900

    add score to GameStats

--- src/teresource/scenes/play/controller/stats.js
+++ src/teresource/scenes/play/controller/stats.js
@@ -1,7 +1,32 @@
 import { LineClearAttackData } from "../core/attack";
 
+/** @param {LineClearAttackData} data */
+function getLineClearScore(data) {
+    let score = 0;
+
+    if(data.isSpecial) {
+        const isSingleMini = data.isMini && data.clearedRowList.length === 1;
+        if(isSingleMini) score += 6;
+        else score += data.clearedRowList.length * 8;
+    } else {
+        score += data.clearedRowList.length ** 2;
+    }
+
+    if(data.B2B) score *= 1.5;
+
+    if(data.combo) {
+        const comboFactor = Math.min(data.combo, 10);
+        score += 0.5 * comboFactor;
+    }
+
+    if(data.isAllClear) score += 50;
+
+    return Math.round(score);
+}
+
 export class GameStats {
-    /** @type {number} */ clearedLines = 0
+    /** @type {number} */ clearedLines = 0;
+    /** @type {number} */ score = 0;
 }
 
 export class GameStatsManager {
@@ -18,7 +43,9 @@ export class GameStatsManager {
     update() {
         if(this.lineClearAttackData) {
             this.stats.clearedLines += this.lineClearAttackData.clearedRowList.length;
-            console.log(`total ${this.stats.clearedLines}`);
+            this.stats.score += getLineClearScore(this.lineClearAttackData);
+
+            console.log(this.stats.score);
         }
 
         //Ready for next frame
