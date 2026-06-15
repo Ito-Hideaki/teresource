36ab848cae0290b0aa1f6f2447ca753618d0445a
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2026-06-15 17:14:38 +0900

    refactor: decouple settings panel creation into reusable function

--- src/teresource/scenes/menu/settingspanel.ts
+++ src/teresource/scenes/menu/settingspanel.ts
@@ -1,18 +1,22 @@
 import { createConfigUIElement } from "../../configUI";
 
-export function createAndAddSettingsPanel() {
-    const { element: panel, configUIDataHandlerMap } = createConfigUIElement(["keyBinding"]);
-    panel.style.position = "absolute";
-    panel.style.top = `0`;
-    panel.style.right = `0`;
-    panel.style.height = `100%`;
-    panel.style.width = `70%`;
+export function createConfigPanel(style: string, categoryKeyList: string[]) {
+    const { element: panel, configUIDataHandlerMap } = createConfigUIElement(categoryKeyList);
+    panel.style = style;
     const normalDisplayStyle = panel.style.display;
-    document.getElementById("outer-game-box")?.appendChild(panel);
+    const container = document.getElementById("outer-game-box");
+    container?.appendChild(panel);
     return {
         setVisible: function(visibility: boolean) {
             panel.style.display = visibility ? normalDisplayStyle : "none";
         },
+        destroyPanel: function() {
+            container?.removeChild(panel);
+        },
         configUIDataHandlerMap
     };
+}
+
+export function createAndAddSettingsPanel() {
+    return createConfigPanel("position: absolute; top: 0; right: 0; height: 100%; width: 70%;", ["keyBinding"]);
 }
