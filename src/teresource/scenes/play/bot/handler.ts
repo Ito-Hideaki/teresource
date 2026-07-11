69bff411f86b76457ca7d24fc3f393be2c5e7ee4
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2026-07-11 21:07:14 +0900

    refactor: decouple TBPHandler from worker using TBPImpl

--- src/teresource/scenes/play/bot/handler.ts
+++ src/teresource/scenes/play/bot/handler.ts
@@ -3,8 +3,51 @@ import { Cell, CellBoard, Mino } from "../core/mechanics";
 import { MinoQueueManager } from "../core/minomanager";
 import { GameContext, GameHighContext } from "../infra/context";
 import { ControlOrder, ControlOrderProvider } from "../controller/controlorder";
+import { viteURLify } from "#util";
 
 export type BotConfig = { type: "test1" | "test2" };
+
+type TBPImpl = {
+    sendMessageObject: (message: any) => void,
+    terminate: () => void,
+    addListener: (message: any) => void
+}
+
+const emptyImpl: TBPImpl = {
+    sendMessageObject: (message: any) => {},
+    terminate: () => {},
+    addListener: (message: any) => {}
+}
+
+const WORKER_PATHS = {
+    "test2": "cc2/worker.js"
+};
+
+function createWorkerImpl(type: keyof typeof WORKER_PATHS): TBPImpl {
+    const workerPath = WORKER_PATHS[type];
+    if(!workerPath) throw "tbp worker path not found";
+    const worker = new Worker(viteURLify(workerPath), { type: "module" });
+    const listeners: Array<(message: any) => any> = [];
+    worker.onmessage = e => {
+        for(const listener of listeners) listener(JSON.parse(e.data));
+    };
+    let terminated = false;
+    return {
+        addListener: function(listener: (message: any) => any) {
+            listeners.push(listener);
+        },
+        terminate: function() {
+            if(!terminated) {
+                worker.terminate();
+                terminated = true;
+            }
+        },
+        sendMessageObject(obj: any) {
+            if(terminated) throw "Error: worker has terminated";
+            worker.postMessage(JSON.stringify(obj));
+        }
+    }
+};
 
 function minoChar(mino: Mino) {
     return mino.type.toUpperCase();
@@ -49,18 +92,31 @@ function sendJson(json: string) {
 export class TBPHandler {
     terminated: boolean;
     private controlOrderProvider;
+    private impl;
 
-    constructor(controlOrderProvider: ControlOrderProvider) {
+    constructor(impl: TBPImpl, controlOrderProvider: ControlOrderProvider) {
         this.terminated = false;
         this.controlOrderProvider = controlOrderProvider;
+        this.impl = impl;
+        this.impl.addListener(this.onMessage.bind(this));
+    }
+
+    onMessage(message: any) {
+
     }
 
     quit() {
         //terminate bot
+        this.impl.terminate();
         this.terminated = true;
     }
 }
 
+function createImpl(type: BotConfig["type"]) {
+    if(type == "test1") return emptyImpl;
+    return createWorkerImpl(type);
+}
+
 export function createTBPHandler(config: BotConfig, gameContext: GameContext, gameHighContext: GameHighContext) {
-    return new TBPHandler(gameHighContext.controlOrderProvider);
+    return new TBPHandler(createImpl(config.type), gameHighContext.controlOrderProvider);
 };
