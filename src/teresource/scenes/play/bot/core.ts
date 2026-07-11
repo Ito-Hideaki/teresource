70ce2f3696e7b14217ca42f9f82c66872218214b
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2026-07-11 22:13:36 +0900

    feat: define TBP bot message as type

---
+++ src/teresource/scenes/play/bot/core.ts
@@ -0,0 +1,30 @@
+export type MinoType = "S" | "T" | "Z" | "I" | "O" | "J" | "L";
+export type Orientation = "east" | "west" | "south" | "north";
+export type Spin = "none" | "full" | "mini";
+
+export type Location = {
+    type: MinoType,
+    orientation: Orientation,
+    x: number,
+    y: number
+};
+
+export type Move = {
+    location: Location,
+    spin: Spin
+};
+
+export type InfoMessage = {
+    type: "info",
+    name: string,
+    version: string,
+    author: string,
+    features: Array<any>
+};
+
+export type SuggestionMessage = {
+    type: "suggestion",
+    moves: Move[]
+};
+
+export type BotMessage = InfoMessage | SuggestionMessage;
--- src/teresource/scenes/play/bot/handler.ts
+++ src/teresource/scenes/play/bot/handler.ts
@@ -4,14 +4,15 @@ import { MinoQueueManager } from "../core/minomanager";
 import { GameContext, GameHighContext } from "../infra/context";
 import { ControlOrder, ControlOrderProvider } from "../controller/controlorder";
 import { viteURLify } from "#util";
+import * as Core from "./core";
 
 export type BotConfig = { type: "test1" | "test2" };
 
 type TBPImpl = {
     sendMessageObject: (message: any) => void,
     terminate: () => void,
-    addListener: (message: any) => void
-}
+    addListener: (listener: (message: Core.BotMessage) => any) => void
+};
 
 const emptyImpl: TBPImpl = {
     sendMessageObject: (message: any) => {},
@@ -27,13 +28,13 @@ function createWorkerImpl(type: keyof typeof WORKER_PATHS): TBPImpl {
     const workerPath = WORKER_PATHS[type];
     if(!workerPath) throw "tbp worker path not found";
     const worker = new Worker(viteURLify(workerPath), { type: "module" });
-    const listeners: Array<(message: any) => any> = [];
+    const listeners: Array<(message: Core.BotMessage) => any> = [];
     worker.onmessage = e => {
         for(const listener of listeners) listener(JSON.parse(e.data));
     };
     let terminated = false;
     return {
-        addListener: function(listener: (message: any) => any) {
+        addListener: function(listener: (message: Core.BotMessage) => any) {
             listeners.push(listener);
         },
         terminate: function() {
@@ -101,7 +102,7 @@ export class TBPHandler {
         this.impl.addListener(this.onMessage.bind(this));
     }
 
-    onMessage(message: any) {
+    onMessage(message: Core.BotMessage) {
 
     }
 
