06aa842f8bef3d53c24d811fc77e0ca47c1694c0
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2026-07-12 20:36:39 +0900

    feat: create node in getNode

--- src/teresource/scenes/play/bot/search.ts
+++ src/teresource/scenes/play/bot/search.ts
@@ -11,3 +11,27 @@ type Node = {
 type NodeCell = { [k: number]: Node };
 type NodeRow = { [k: number]: NodeCell };
 type NodeBoard = { [k: number]: NodeRow };
+
+class NodeUtility {
+    private rowCount;
+    private columnCount;
+    board: NodeBoard;
+
+    constructor(gameContext: GameContext) {
+        this.rowCount = gameContext.cellBoard.rowCount;
+        this.columnCount = gameContext.cellBoard.columnCount;
+        this.board = {};
+    }
+
+    getNode(location: TRS.Location, parent: Node) {
+
+        this.board[location.y] ??= {};
+        this.board[location.y][location.x] ??= {};
+        this.board[location.y][location.x][location.rotation] ??= {
+            location: { ...location },
+            depth: parent.depth + 1,
+            parent
+        };
+        return this.board[location.y][location.x][location.rotation];
+    }
+}
