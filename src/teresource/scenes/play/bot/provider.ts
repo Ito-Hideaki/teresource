e2d2d2fb472e5c7da0e0f1668e8ae4c397cb4998
Ito-Hideaki <s24023@tokyo.kosen-ac.jp>
2026-07-17 16:30:19 +0900

    feat: do underground rotation

--- src/teresource/scenes/play/bot/provider.ts
+++ src/teresource/scenes/play/bot/provider.ts
@@ -1,13 +1,15 @@
 import { BoardUpdateDiff } from "../controller/boardcontroller";
 import { ControlOrder, ControlOrderGateway } from "../controller/controlorder";
 import { GameContext } from "../infra/context";
-import { Node, Path } from "./search";
+import { BotOrder, Node, Path } from "./search";
+import * as TRS from "./trscore";
 
 export class BotControlOrderProvider {
 
     private pathQueue: Path[] = [];
     private currentNode: Node | undefined;
     private currentMinoManager;
+    private reachedFirstNode = false;
 
     constructor(gameContext: GameContext, controlOrderGateway: ControlOrderGateway) {
         controlOrderGateway.setProvider(this);
@@ -26,7 +28,13 @@ export class BotControlOrderProvider {
     private place() {
         this.pathQueue.splice(0, 1);
         this.currentNode = undefined;
+        this.reachedFirstNode = false;
         return new ControlOrder(ControlOrder.HARD_DROP);
+    }
+
+    private isOnLocation(location: TRS.Location) {
+        console.log(location, this.currentMinoManager.mino.rotation);
+        return this.currentMinoManager.row === location.y && this.currentMinoManager.column === location.x && this.currentMinoManager.mino.rotation === location.rotation;
     }
 
     provideControlOrder() {
@@ -37,7 +45,7 @@ export class BotControlOrderProvider {
 
         const firstNode = this.pathQueue[0].route.at(0) ?? this.pathQueue[0].goal;
         const rotationDiff = (node.location.rotation - this.currentMinoManager.mino.rotation + 360) % 360;
-        if(node === firstNode) {
+        if(!this.reachedFirstNode && node === firstNode) {
             if(rotationDiff) {
                 return new ControlOrder(rotationDiff > 180 ? ControlOrder.ROTATE_COUNTER_CLOCK : ControlOrder.ROTATE_CLOCK_WISE);
             } else if(node.location.x !== this.currentMinoManager.column) {
@@ -45,13 +53,19 @@ export class BotControlOrderProvider {
                 return new ControlOrder(isRight ? ControlOrder.MOVE_RIGHT : ControlOrder.MOVE_LEFT);
             } else if(!node.parent) return this.place();
             else if (node.location.y !== this.currentMinoManager.row) return new ControlOrder(ControlOrder.START_SOFT_DROP);
-            else {
+            else this.reachedFirstNode = true;
+        }
+
+        if(node.parent) {
+            if(this.isOnLocation(node.parent.location)) {
                 this.currentNode = node.parent;
                 return this.provideControlOrder();
+            } else switch (node.controlToParent) {
+                case BotOrder.ROTATE_CLOCK_WISE: return new ControlOrder(ControlOrder.ROTATE_CLOCK_WISE);
+                case BotOrder.ROTATE_COUNTER_CLOCK: return new ControlOrder(ControlOrder.ROTATE_COUNTER_CLOCK);
             }
-        } else if(node.parent) {
-
         } else {
+
         }
 
         return new ControlOrder(0);
